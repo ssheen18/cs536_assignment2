8970cf5bfc89ab61975988a623056c31f24a86e4
ssheen18 <sheen2@wisc.edu>
2018-11-08 16:43:08 -0600

    Added door opening.

---
+++ Assets/doorOpen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpen : MonoBehaviour {
+    bool opened = false;
+	private void OnTriggerEnter(Collider other)
+    {
+        if (opened == false)
+        {
+            transform.Rotate(0, 0, -90);
+            opened = true;
+        } 
+    }
+
+}
